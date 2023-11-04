a0622772fc574217331ad71327c9f0039c3c9382
Joshua Loui Soqueno <80311305+alphapuppey2@users.noreply.github.com>
2023-11-04 11:45:19 +0800

    tailwind and Login

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # TS_Components
- This will include the Following Login SideBar NavBar etc
+ This will include the Following Components 
+ in this Repository this will use Typescript with Tailwind
+    Login
+    SideBar
+    NavBar etc...
---
+++ web-app/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Login from './component/Login';
+
+function App() {
+  return (
+    <div className="App border-box w-screen h-screen flex justify-center items-center">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
