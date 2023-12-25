a89a617ee5a6a57a56bd12e2a646ad947713f66a
joshua loui Soqueno <alphapuppey@gmail.com>
2023-12-25 22:48:52 +0800

    c

--- inventorySystem/web-app/src/component/SideBar.tsx
+++ inventorySystem/web-app/src/component/SideBar.tsx
@@ -25,7 +25,7 @@ function RouteLocs() {
 const SideBar = () => {
     return (
         <>
-
+            {RouteLocs}
             <div className="fixed z-50 left-0 top-0 h-full  w-[15rem]">
                 <IconLinker to={"/dashboard"} Icon={RiDashboardLine}>Dashboard</IconLinker>
                 <IconLinker to={"/calendar"} Icon={FaCalendarAlt}>calendar</IconLinker>
---
+++ inventorySystem/web-app/src/configFirebase/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyANWqSlFO_MGpZSt_Y_Jw42cTX4FHptdSA",
+  authDomain: "inventoryapp-674f1.firebaseapp.com",
+  projectId: "inventoryapp-674f1",
+  storageBucket: "inventoryapp-674f1.appspot.com",
+  messagingSenderId: "410644775211",
+  appId: "1:410644775211:web:d3a4be65c5cc50a4ec856e",
+  measurementId: "G-8FMD2T3L8B"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
